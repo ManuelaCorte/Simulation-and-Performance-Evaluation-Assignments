c609e8249242cfbc7f682f8bc90b6a76a79aeeb3
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-05-16 15:22:22 +0200

    test with multiple server queues

--- Assignment_3/utils/event.py
+++ Assignment_3/utils/event.py
@@ -4,15 +4,18 @@ from queue import PriorityQueue
 
 
 class Event:
-    def __init__(self, type, time, idx=None):
+    def __init__(self, type, time, idx = None, server = -1):
         self.type = type
         self.time = time
         self.idx = idx
+        if (type == EventType.departure and server < 0):
+            raise "Error: departure set without a specified server"
+        self.server = server
 
     def __str__(self) -> str:
         if self.type == EventType.arrival or self.type == EventType.departure:
-            return f"Event: {self.type} of packet {self.idx} at {self.time}"
-        return f"Event: {self.type}  at {self.time}"
+            return f"Event: {self.type} of packet {self.idx} on server {self.server} at {self.time}"
+        return f"Event: {self.type} on server {self.server} at {self.time}"
 
 
 EventType = Enum("Type", "start arrival departure stop debug")
@@ -21,8 +24,8 @@ EventType = Enum("Type", "start arrival departure stop debug")
 class EventQueue:
     def __init__(self, simulation_time):
         self.queue = PriorityQueue()
-        self.queue.put((0.0, Event(EventType.start, 0, None)))
-        self.queue.put((simulation_time, Event(EventType.stop, simulation_time, None)))
+        self.queue.put((0.0, Event(EventType.start, 0)))
+        self.queue.put((simulation_time, Event(EventType.stop, simulation_time)))
 
     def __str__(self) -> str:
         string = ""
