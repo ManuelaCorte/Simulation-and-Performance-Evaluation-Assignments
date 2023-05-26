4ff988a377837b30dc2f27271206a30a957ab6ee
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-05-26 18:10:28 +0200

    double

--- Assignment_4/fn/get_data_from_csv.py
+++ Assignment_4/fn/get_data_from_csv.py
@@ -4,12 +4,12 @@ filename = 'theory_ex_flooding.csv'
 
 def get_data_from_csv():
     p_axis = []
-    d_zero_2_axis = []
-    d_zero_5_axis = []
+    d_zero_msg_2_expected_axis = []
+    d_zero_msg_5_expected_axis = []
     with open(filename, newline='') as csvfile:
       reader = csv.reader(csvfile, delimiter=',')
       for row in reader:
         p_axis.append(float(row[0]))
-        d_zero_2_axis.append(float(row[1]))
-        d_zero_5_axis.append(float(row[2]))
-    return p_axis, d_zero_2_axis, d_zero_5_axis
+        d_zero_msg_2_expected_axis.append(float(row[1]))
+        d_zero_msg_5_expected_axis.append(float(row[2]))
+    return p_axis, d_zero_msg_2_expected_axis, d_zero_msg_5_expected_axis
--- Assignment_4/index.py
+++ Assignment_4/index.py
@@ -4,22 +4,35 @@ from fn.multiple_sim import multiple_sim
 from fn.get_data_from_csv import get_data_from_csv
 
 gen = np.random.default_rng(seed=41)
-r = 5
-N = 5
 
-p_axis, d_zero_2_axis, d_zero_5_axis = get_data_from_csv()
+p_axis, d_zero_msg_2_expected_axis, d_zero_msg_5_expected_axis = get_data_from_csv()
 
 runs = 10000
 n_steps = 10
 
-y = []
+d_zero_msg_2_sim_axis = []
+d_zero_msg_5_sim_axis = []
 
+r = 2
+N = 2
 for index, p in enumerate(p_axis):
   runs_when_d_is_zero = multiple_sim(gen, r, N, p, runs)
   runs_when_d_is_zero_perc = runs_when_d_is_zero / runs
-  print(f"p error = {p:.3f}, p no msg arrived expect = {d_zero_5_axis[index]:.3f}, sim = {runs_when_d_is_zero_perc:.3f}")
-  y.append(runs_when_d_is_zero_perc)
+  print(f"p error = {p:.3f}, p no msg arrived expect = {d_zero_msg_2_expected_axis[index]:.3f}, sim = {runs_when_d_is_zero_perc:.3f}")
+  d_zero_msg_2_sim_axis.append(runs_when_d_is_zero_perc)
 
-plt.plot(p_axis, y)
-plt.plot(p_axis, d_zero_5_axis)
+r = 5
+N = 5
+for index, p in enumerate(p_axis):
+  runs_when_d_is_zero = multiple_sim(gen, r, N, p, runs)
+  runs_when_d_is_zero_perc = runs_when_d_is_zero / runs
+  print(f"p error = {p:.3f}, p no msg arrived expect = {d_zero_msg_5_expected_axis[index]:.3f}, sim = {runs_when_d_is_zero_perc:.3f}")
+  d_zero_msg_5_sim_axis.append(runs_when_d_is_zero_perc)
+
+plt.figure()
+plt.plot(p_axis, d_zero_msg_2_sim_axis)
+plt.plot(p_axis, d_zero_msg_2_expected_axis)
+plt.figure()
+plt.plot(p_axis, d_zero_msg_5_sim_axis)
+plt.plot(p_axis, d_zero_msg_5_expected_axis)
 plt.show()
