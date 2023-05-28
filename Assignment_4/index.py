37da37ff8facc42ee8ddc63aea0769ee3b9fca52
ManuelaCorte <manu.cortepause@gmail.com>
2023-05-28 10:40:14 +0200

    Added labels to plots

--- Assignment_4/index.py
+++ Assignment_4/index.py
@@ -8,7 +8,6 @@ gen = np.random.default_rng(seed=41)
 p_axis, d_zero_msg_2_expected_axis, d_zero_msg_5_expected_axis = get_data_from_csv()
 
 runs = 10000
-n_steps = 10
 
 d_zero_msg_2_sim_axis = []
 d_zero_msg_5_sim_axis = []
@@ -29,10 +28,20 @@ for index, p in enumerate(p_axis):
   print(f"p error = {p:.3f}, p no msg arrived expect = {d_zero_msg_5_expected_axis[index]:.3f}, sim = {runs_when_d_is_zero_perc:.3f}")
   d_zero_msg_5_sim_axis.append(runs_when_d_is_zero_perc)
 
-plt.figure()
-plt.plot(p_axis, d_zero_msg_2_sim_axis)
-plt.plot(p_axis, d_zero_msg_2_expected_axis)
-plt.figure()
-plt.plot(p_axis, d_zero_msg_5_sim_axis)
-plt.plot(p_axis, d_zero_msg_5_expected_axis)
+f, ax = plt.subplots(1, 1, figsize=(10,10))
+ax.plot(p_axis, d_zero_msg_2_sim_axis, label='simulated value')
+ax.plot(p_axis, d_zero_msg_2_expected_axis, label='theoretical value')
+ax.set_title('error rates, r = 2, N = 2')
+ax.set_xlabel('p')
+ax.set_ylabel('p no message arrived to D')
+ax.legend()
+
+f, ax = plt.subplots(1, 1, figsize=(10,10))
+ax.plot(p_axis, d_zero_msg_5_sim_axis, label='simulated value')
+ax.plot(p_axis, d_zero_msg_5_expected_axis, label='theoretical value')
+ax.set_title('error rates, r = 5, N = 5')
+ax.set_xlabel('p')
+ax.set_ylabel('p no message arrived to D')
+ax.legend()
+
 plt.show()
