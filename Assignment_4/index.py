a250dff673f52b7a08e07a5df2c48e8b9b21c3cb
ManuelaCorte <manu.cortepause@gmail.com>
2023-05-31 17:43:53 +0200

    Error plots added for main simulation

--- Assignment_4/index.py
+++ Assignment_4/index.py
@@ -5,10 +5,12 @@ from fn.get_data_from_csv import get_data_from_csv
 
 p_axis, d_zero_msg_2_expected_axis, d_zero_msg_5_expected_axis = get_data_from_csv()
 
-runs = 10000
+runs = 1000
 
 d_zero_msg_2_sim_axis = []
 d_zero_msg_5_sim_axis = []
+msg_2_sim_ci = []
+msg_5_sim_ci = []
 results_2_sim = {}
 results_5_sim = {}
 
@@ -20,6 +22,7 @@ for index, p in enumerate(p_axis):
         f"p error = {p:.3f}, p no msg arrived expect = {d_zero_msg_2_expected_axis[index]:.3f}, sim = {results_2_sim.total_mean:.3f} +- {results_2_sim.ci_total_mean:.3f}"
     )
     d_zero_msg_2_sim_axis.append(results_2_sim.total_mean)
+    msg_2_sim_ci.append(results_2_sim.ci_total_mean)
 
 r = 5
 N = 5
@@ -29,10 +32,19 @@ for index, p in enumerate(p_axis):
         f"p error = {p:.3f}, p no msg arrived expect = {d_zero_msg_5_expected_axis[index]:.3f}, sim = {results_5_sim.total_mean:.3f} +- {results_5_sim.ci_total_mean:.3f}"
     )
     d_zero_msg_5_sim_axis.append(results_5_sim.total_mean)
+    msg_5_sim_ci.append(results_5_sim.ci_total_mean)
 
 
 f, ax = plt.subplots(1, 1, figsize=(10, 10))
-ax.plot(p_axis, d_zero_msg_2_sim_axis, label="simulated value")
+ax.errorbar(
+    p_axis,
+    d_zero_msg_2_sim_axis,
+    yerr=msg_2_sim_ci,
+    linestyle="dotted",
+    marker="o",
+    markersize=2,
+    label="simulated value",
+)
 ax.plot(p_axis, d_zero_msg_2_expected_axis, label="theoretical value")
 ax.set_title("error rates, r = 2, N = 2")
 ax.set_xlabel("p")
@@ -40,7 +52,15 @@ ax.set_ylabel("p no message arrived to D")
 ax.legend()
 
 f, ax = plt.subplots(1, 1, figsize=(10, 10))
-ax.plot(p_axis, d_zero_msg_5_sim_axis, label="simulated value")
+ax.errorbar(
+    p_axis,
+    d_zero_msg_5_sim_axis,
+    yerr=msg_5_sim_ci,
+    linestyle="dotted",
+    marker="o",
+    markersize=2,
+    label="simulated value",
+)
 ax.plot(p_axis, d_zero_msg_5_expected_axis, label="theoretical value")
 ax.set_title("error rates, r = 5, N = 5")
 ax.set_xlabel("p")
